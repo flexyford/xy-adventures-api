bccb63ec1df91a677bf398fc214a0961a11cb362
Flexy Ford <flexondatrack@gmail.com>
2015-02-13 22:26:45 -0600

    reduce num workers to 1

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,6 @@
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+workers Integer(ENV['WEB_CONCURRENCY'] || 1)
 threads_count = Integer(ENV['MAX_THREADS'] || 16)
-threads threads_count, threads_count
+threads 1, threads_count
 
 preload_app!
 
