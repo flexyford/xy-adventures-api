77b7bed0021742341668b2fbaaa0f07cf44955f6
Flexy Ford <flexondatrack@gmail.com>
2015-02-16 22:17:59 -0600

    try allowing *

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module XyAdventuresApi
     config.active_record.raise_in_transactional_callbacks = true
 
     config.action_dispatch.default_headers.merge!({
-     'Access-Control-Allow-Origin' => 'http://localhost:4567',
+     'Access-Control-Allow-Origin' => '*',
      'Access-Control-Request-Method' => '*'
     })
       
